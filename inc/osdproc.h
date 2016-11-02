722a240cb2dcff23ca16acb927c6325a882bdca4
Stewart Loving-Gibbard <stew@lovinggibbard.com>
2016-11-01 22:39:16 -0700

    This attempts to add concurrency protection by using FreeRTOS Mutexes to protect access to global variables, under the assumption that unprotected access may be causing difficult to reproduce, erratic errors.
    I am not completely confident that these fixes repair problems we've had with PlayUAV (like bad home direction or erratic altitude), but it seems worth a shot, and is arguably better.

--- inc/osdproc.h
+++ inc/osdproc.h
@@ -64,7 +64,6 @@ void draw_wind(void);
 void draw_map(void);
 void draw_panel_changed(void);
 void draw_warning(void);
-void draw_CWH(void);
 void draw_head_wp_home(void);
 void draw_watts(void);
 void draw_osd_linear_compass(void);
